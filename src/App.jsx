e992d016f4d6669ebf9cae1541af4ac6a42bda03
K. P. Sreedhar <sreedharkp2006@gmail.com>
2026-02-08 21:56:49 +0530

    Replace BrowserRouter with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import PageLayout from './components/PageLayout'
 import { AppProvider } from './context/AppContext'
 import AppRoutes from './routes/AppRoutes'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppProvider>
         <PageLayout>
           <AppRoutes />
         </PageLayout>
       </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
